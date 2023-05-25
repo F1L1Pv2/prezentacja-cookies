9da434ea9c69da1636d9441b059be0f537c0446e
F1L1P <mlodzik.filip@wp.pl>
2023-05-25 10:06:08 +0200

    minor update

--- index.php
+++ index.php
@@ -6,13 +6,15 @@
   if (!isset($_COOKIE[$cookie_name])) {
     echo "O NIE nie ma ciasteczka kliknij ten przycisk aby je stworzyć";
     echo "<form action='ciastko.php' method='get'><input type='text' name='wartosc' placeholder='imie...'><button type='submit' name='cozrobic' value='ustaw'>Ustaw ciasteczko</button></form>";
-
+    
   } else {
     echo "<h1>Witaj: " . $_COOKIE[$cookie_name] . "!</h1>";
     echo "Aby usunąć ciasteczko kliknij ten przycisk";
     echo "<form action='ciastko.php' method='get'><button type='submit' name='cozrobic' value='usun'>Usuń ciasteczko</button></form>";
   }
   ?>
+  
+  <h4>Link do prezentacji: <a href="./prezentacja.pdf">Kliknij tutaj</a></h4>
 
 </body>
 
